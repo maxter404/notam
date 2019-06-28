d58c3bce8f26ff2902f9d9e506105183b4ad15a0
maxter404 <m_maxpower@hotmail.com>
2019-06-27 22:58:30 -0500

    mobile y desktop responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    alert("holiwi");
+    $('#serviciosSlider').slick({
+        slide: '.slideServicios',        
+        infinite: true,
+        slidesToShow: 1,
+        rows: 0,
+        arrows: false,
+        dots: true
+      });
+      $(".linkedin").on("click", ()=>{
+        location.href = "www.google.com";
+      });
+      $(".instagram").on("click", ()=>{
+        location.href = "www.google.com";
+      });
+      $(".facebook").on("click", ()=>{
+        location.href = "www.google.com";
+      });
 });
