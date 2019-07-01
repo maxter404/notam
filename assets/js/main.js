45f885c4a8f5aa16fcda3dc4866cb608c5c5236c
maxter404 <m_maxpower@hotmail.com>
2019-07-01 16:03:21 -0500

    cambios de rutas y detalles menores

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,17 @@ $(document).ready(function(){
         dots: true
       });
       $(".linkedin").on("click", ()=>{
-        location.href = "www.google.com";
+        location.href = "https://www.linkedin.com/company/notammx";
       });
       $(".instagram").on("click", ()=>{
-        location.href = "www.google.com";
+        location.href = "https://www.instagram.com/notam.mx/";
       });
       $(".facebook").on("click", ()=>{
-        location.href = "www.google.com";
+        location.href = "https://www.facebook.com/notam.mx/";
+      });
+      $(".purpleBtn").on("click", ()=>{
+        $('html, body').animate({
+          scrollTop: $(".somosSection").offset().top
+      }, 2000);
       });
 });
